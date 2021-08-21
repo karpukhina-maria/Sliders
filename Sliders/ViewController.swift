8a97ea977c1de2a9d583e5412de3e5778489574b
Maria Karpukhina <myshka_n@mail.ru>
2021-08-21 11:51:37 +0300

    Logic without constraint

--- Sliders/ViewController.swift
+++ Sliders/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 15
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
+        updateTextSliderLabel()
+        changeViewColor()
+   }
+
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        changeViewColor()
+        updateTextSliderLabel()
     }
-
-
+    
+    func changeViewColor() {
+        colorView.backgroundColor = UIColor(
+            displayP3Red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+    
+    func updateTextSliderLabel() {
+        redValueLabel.text = String(redSlider.value)
+        greenValueLabel.text = String(greenSlider.value)
+        blueValueLabel.text = String(blueSlider.value)
+    }
 }
 
